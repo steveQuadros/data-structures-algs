4886ecf31585632643d31e2463a4cb0b8df12da5
Steve Quadros <stevequadros@gmail.com>
2022-02-22 14:54:44 -0500

    added basic doubly linked list implementation

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,74 @@
+package linkedlist
+
+import "strings"
+
+type DoublyLinkedListNode struct {
+	Prev, Next *DoublyLinkedListNode
+	Val        string
+}
+
+func NewDoublyLinkedListNode(s string) DoublyLinkedListNode {
+	return DoublyLinkedListNode{Val: s}
+}
+
+type DoublyLinkedList struct {
+	dummyHead, dummyTail *DoublyLinkedListNode
+}
+
+func NewDoublyLinkedList() DoublyLinkedList {
+	head := &DoublyLinkedListNode{}
+	tail := &DoublyLinkedListNode{}
+	head.Next = tail
+	tail.Prev = head
+	return DoublyLinkedList{
+		dummyHead: head,
+		dummyTail: tail,
+	}
+}
+
+func (d *DoublyLinkedList) PushFront(node *DoublyLinkedListNode) {
+	oldHead := d.dummyHead.Next
+	d.dummyHead.Next = node
+	node.Next = oldHead
+	node.Prev = d.dummyHead
+	oldHead.Prev = node
+}
+
+func (d *DoublyLinkedList) PushBack(node *DoublyLinkedListNode) {
+	oldTail := d.dummyTail.Prev
+	node.Next = d.dummyTail
+	node.Prev = oldTail
+	oldTail.Next = node
+	d.dummyTail.Prev = node
+}
+
+func (d *DoublyLinkedList) Remove(node *DoublyLinkedListNode) {
+	node.Prev.Next = node.Next
+	node.Next.Prev = node.Prev
+	node = nil
+}
+
+func (d *DoublyLinkedList) IsEmpty() bool {
+	return d.dummyHead == d.dummyTail
+}
+
+func (d *DoublyLinkedList) Front() *DoublyLinkedListNode {
+	return d.dummyHead.Next
+}
+
+func (d *DoublyLinkedList) Back() *DoublyLinkedListNode {
+	return d.dummyTail.Prev
+}
+
+func (d *DoublyLinkedList) String() string {
+	node := d.dummyHead.Next
+	b := strings.Builder{}
+	for node != nil {
+		b.WriteString(node.Val)
+		node = node.Next
+		if node != nil {
+			b.WriteString(" <-> ")
+		}
+	}
+	return b.String()
+}
---
+++ linkedlist/linkedlist_test.go
@@ -0,0 +1,74 @@
+package linkedlist
+
+import (
+	"github.com/stretchr/testify/require"
+	"strconv"
+	"testing"
+)
+
+func TestDoublyLinkedList(t *testing.T) {
+	ddl := NewDoublyLinkedList()
+
+	one := NewDoublyLinkedListNode("1")
+	ddl.PushBack(&one)
+	require.Equal(t, &one, ddl.Front())
+	require.Equal(t, &one, ddl.Back())
+
+	two := NewDoublyLinkedListNode("2")
+	ddl.PushBack(&two)
+	require.Equal(t, &one, ddl.Front())
+	require.Equal(t, &two, ddl.Back())
+
+	three := NewDoublyLinkedListNode("3")
+	ddl.PushBack(&three)
+	require.Equal(t, &one, ddl.Front())
+	require.Equal(t, &three, ddl.Back())
+
+	// remove head
+	ddl.Remove(&one)
+	require.Equal(t, &two, ddl.Front())
+	require.Equal(t, &three, ddl.Back())
+
+	// remove tail
+	ddl.Remove(&two)
+	require.Equal(t, &three, ddl.Front())
+	require.Equal(t, &three, ddl.Back())
+
+	// pushFront
+	four := NewDoublyLinkedListNode("4")
+	ddl.PushFront(&four)
+	require.Equal(t, &four, ddl.Front())
+	require.Equal(t, &three, ddl.Back())
+
+	// pushFront
+	five := NewDoublyLinkedListNode("5")
+	ddl.PushFront(&five)
+	require.Equal(t, &five, ddl.Front())
+	require.Equal(t, &three, ddl.Back())
+}
+
+func TestDoublyLinkedList_PushFront(t *testing.T) {
+	ddl := NewDoublyLinkedList()
+	var nodes []DoublyLinkedListNode
+	for i := 0; i < 10; i++ {
+		n := NewDoublyLinkedListNode(strconv.Itoa(i))
+		nodes = append(nodes, n)
+		ddl.PushFront(&n)
+	}
+
+	require.Equal(t, nodes[9].Val, ddl.Front().Val, ddl.String())
+	require.Equal(t, nodes[0].Val, ddl.Back().Val, ddl.String())
+}
+
+func TestDoublyLinkedList_PushBack(t *testing.T) {
+	ddl := NewDoublyLinkedList()
+	var nodes []DoublyLinkedListNode
+	for i := 0; i < 10; i++ {
+		n := NewDoublyLinkedListNode(strconv.Itoa(i))
+		nodes = append(nodes, n)
+		ddl.PushBack(&n)
+	}
+
+	require.Equal(t, nodes[0].Val, ddl.Front().Val, ddl.String())
+	require.Equal(t, nodes[9].Val, ddl.Back().Val, ddl.String())
+}
